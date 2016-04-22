58ea0ee9c47c547ed4e41bae3607fdc5152c9685
Ben Weeks <bweeks@gmail.com>
2016-04-21 23:57:56 -0400

    Hmmm... safari doesn't seem to like this.

--- site/static/js/shop.js
+++ site/static/js/shop.js
@@ -5,24 +5,34 @@ function Shopping() {
     this.purchaseList = new ItemList();
     console.log("purchaseList created");
     this.shoppingLists = indexedDB.open("shoppingLists", 1);
+    this.db = undefined;
     this.lists = indexedDB.open("lists", 1);
     this.lists.onupgradeneeded = function(e) {
-        var thisDB = e.target.result;
+        var listDB = e.target.result;
         console.log("running onupgradeneeded for lists");
-        if(!thisDB.objectSotreNames.contains("shoppingLists")) {
+        if(!listDB.objectStoreNames.contains("shoppingLists")) {
             console.log("making object store for shoppingLists");
-            thisDB.createdObjectStore("shoppingLists");
+            listDB.createdObjectStore("shoppingLists", {keyPath:"id", autoIncrement: true});
         }
-        if(!thisDB.objectSotreNames.contains("purchaseLists")) {
+        if(!listDB.objectStoreNames.contains("purchaseLists")) {
             console.log("making object store for purchaseLists");
-            thisDB.createdObjectStore("purchaseLists");
+            listDB.createdObjectStore("purchaseLists", {keyPath:"id", autoIncrement: true});
         }
     }
+    this.lists.onsuccess = function(e) {
+        console.log("running onsuccess");
+        this.db = e.target.result;
+        console.dir(this.db.objectStoreNames);
+    }
 
+    this.lists.onerror = function(e) {
+        console.log("onerror!");
+        console.dir(e);
+    }
 }
 
 Shopping.prototype.drawList = function($, list, listId) {
-    var table = $("table." + listId);
+    var table = $("table#" + listId);
     table.children("tbody").remove();
     table.append("<tbody />");
     for (var i = 0; i < list.length; i++) {
@@ -61,10 +71,10 @@ Shopping.prototype.clearList = function($, id) {
 Shopping.prototype.addItem = function($) {
     var item;
     item = {
-        "name" : $(".itemName>input").val(),
-        "priority" : parseInt($(".itemPriority>input").val()),
-        "price" : parseFloat($(".itemPrice>input").val().replace("$", "")),
-        "quantity" : parseInt($(".itemQuantity>input").val())
+        "name" : $("#itemName").val(),
+        "priority" : parseInt($("#itemPriority").val()),
+        "price" : parseFloat($("#itemPrice").val().replace("$", "")),
+        "quantity" : parseInt($("#itemQuantity").val())
     }
     try {
         this.shoppingList.addItem(item);
@@ -81,6 +91,9 @@ Shopping.prototype.addItem = function($) {
 Shopping.prototype.removeItem = function(index) {
     this.shoppinglist.removeItem(index);
 };
+
+Shopping.prototype.editItem = function($, index) {
+}
 
 Shopping.prototype.shop = function($) {
     var shoppingList = this.shoppingList.list;
@@ -108,11 +121,69 @@ Shopping.prototype.shop = function($) {
 }
 
 Shopping.prototype.storeList = function($, listID) {
+    var transaction = this.db.transaction([listID+"s"], "readwrite");
 
+    var store = transaction.objectStore(listID+"s")
+
+    if (listID == "shoppingList") {
+        var name = $("#sListSelector").val();
+        var list = this.shoppingList.list;
+    }
+    else {
+        var name = $("pListSelector").val();
+        var list = this.purchaseList.list;
+    }
+
+    var entry = {
+        name: name,
+        created: new Date().getTime(),
+        list: list
+    }
+    var request = store.add(entry);
+ 
+    request.onerror = function(e) {
+        console.log("Error", e.target.error.name);
+    }
+
+    request.onsuccess = function(e) {
+        console.log("Added list " + name);
+    }
 
 }
 
 Shopping.prototype.loadList = function($, listID) {
+    var transaction = this.db.transaction([listID+"s"], "readonly");
+
+    var store = transaction.objectStore(listID+"s")
+
+    if (listID == "shoppingList") {
+        var key = $("#sListSelector").data("key");
+    }
+    else {
+        var key = $("pListSelector").data("key");
+    }
+
+    var request = store.get(Number(key));
+
+    request.onsuccess = function(e) {
+        var result = e.target.result;
+        console.log("Got list!")
+    }
+
+    request.onerror = function(e) {
+        console.log("Error");
+    }
+
+    if (listID == "shoppingList") {
+        this.shoppingList.list = result.list;
+
+    }
+    else {
+        this.purchaseList.list = result.list;
+    }
+
+    this.drawList($, this.shoppingList.list, "shoppingList");
+    this.drawList($, this.purchaseList.list, "purchaseList");
 
 }
 
